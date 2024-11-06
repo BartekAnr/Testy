f70a5975dfcde7e2f352cfc79d11a07884ef09ad
Bartosz Andrzejewski <ezioayr@gmail.com>
2024-11-06 22:42:37 +0100

    Delete Zadania-Zbiory/Zad8.py

--- Zadania-Zbiory/Zad8.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: latin-1 -*-
-# Stwórz listê zawieraj¹c¹ duplikaty, a nastêpnie przekszta³æ j¹ w zbiór, aby uzyskaæ listê unikalnych elementów.
-
-duplicat = [1, 2, 2, 3, 4, 5, 6, 2, 3, 6, 4, 8, 9, 2, 6, 3, 4, 8]
-
-unics = set(duplicat)
-print(unics)
