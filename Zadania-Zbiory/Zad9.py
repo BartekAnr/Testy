dde300f9c19b3e487a372b708b67613a016caa80
Bartosz Andrzejewski <ezioayr@gmail.com>
2024-11-06 22:42:42 +0100

    Delete Zadania-Zbiory/Zad9.py

--- Zadania-Zbiory/Zad9.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: latin-1 -*-
-# Maj¹c listê liczb, stwórz zbiór zawieraj¹cy tylko te liczby, które s¹ wiêksze od 10.
-
-numbers = [12, 222, 4, 56, 3, 67, 10, 11, 43, 1, 0, 65, 203, 148, 5]
-
-unics = set([i for i in numbers if i > 10])
-print(unics)
