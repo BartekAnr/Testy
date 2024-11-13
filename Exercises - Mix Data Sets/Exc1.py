7cbba2f702b64c99b49672053f7a34b489ae1b91
Bartosz Andrzejewski <ezioayr@gmail.com>
2024-11-13 20:58:24 +0100

    Add files via upload

---
+++ Exercises - Mix Data Sets/Exc1.py
@@ -0,0 +1,33 @@
+school = {"Class A":
+            {"English":
+                ["John", "Helen", "Peter", "Joseph"],
+             "Polish":
+                ["Helen", "Bert", "Jospeh", "Jim"],
+             "History":
+                ["Jim", "Bert", "John", "Peter"],
+             "Music":
+                ["Helen", "Jopseh"]
+            },
+          "Class B":
+            {"English":
+                ["Jimi", "Golden", "Hizzy", "Woo"],
+             "Polish":
+                ["Woo", "Belik", "Hizzy", "Conny"],
+             "History":
+                ["Eren", "Fill", "Woo", "Golden"],
+             "Music":
+                ["Jimi", "Heni"]
+            },
+          "Class C":
+            {"English":
+                ["Moly", "Bitsy", "Paol", "Celin"],
+             "Polish":
+                ["Peny", "Kitty", "Bitsy", "Felix"],
+             "History":
+                ["Moly", "Paol", "Felix", "Kitty"],
+             "Music":
+                ["Moly", "Sily"]
+            }
+         }
+
+print(school)
